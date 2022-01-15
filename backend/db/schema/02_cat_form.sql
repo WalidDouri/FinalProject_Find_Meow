c5ef27cd3e0fae3c8aaa9337e311148d5770fe6b
Mei Nghiem-Lin <mcnghiem@gmail.com>
2022-01-15 06:14:17 +0000

    update tables and create  comments table

--- backend/db/schema/02_cat_form.sql
+++ backend/db/schema/02_cat_form.sql
@@ -1,26 +1,19 @@
 DROP TABLE IF EXISTS cat_form CASCADE;
 
--- CREATE TABLE posts(
---     id SERIAL PRIMARY KEY NOT NULL,
---     title VARCHAR(255) NOT NULL,
---     content TEXT,
---     user_id INTEGER REFERENCES users(id)
--- );
-
 CREATE TABLE cat_form (
 
     id SERIAL PRIMARY KEY NOT NULL,
     user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   
-    description TEXT,
+    description VARCHAR(500) NOT NULL,
     image TEXT,
-    cat_name VARCHAR(255),
-    gender VARCHAR(255),
+    cat_name VARCHAR(255) NOT NULL,
+    gender VARCHAR(255) NOT NULL,
     last_seen_date DATE NOT NULL,
     last_seen_address VARCHAR(255) NOT NULL,
     last_seen_city VARCHAR(255) NOT NULL,
     last_seen_postal_code VARCHAR(255) NOT NULL,
     date_created TIMESTAMP NOT NULL DEFAULT NOW(),
-    status VARCHAR(355 NOT NULL)
+    status VARCHAR(20) NOT NULL
 
 ); 
