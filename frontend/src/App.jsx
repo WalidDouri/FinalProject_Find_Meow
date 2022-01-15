6ed42c7673ecb38f7423b2c00bfa1aae98e5d982
Walid <walid_douri@hotmail.com>
2022-01-15 11:55:22 -0500

    Created additional components and corrections

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,31 @@
+import React from 'react';
 import './App.css';
 // import useApplicationData from './hooks/useApplicationData';
 // import users from './components/users';
 import Navbar from './components/Navbar';
-import About from './Pages/About';
-// import Report_Pet from './components/Report_Pet';
-// import Search_Pet from './components/Search_Pet';
-// // import Login from './components/Login';
-// import Form from './components/Form';
 
+
+import About from './Pages/About';
+import Home from './Pages/Home';
+import ReportPet from './Pages/ReportPet';
+import SearchPet from './Pages/SearchPet';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export default function App() {
 
     return (
-      
+
       <BrowserRouter>
-        <Routes>
-          {/* Nested Routes */}
-          <Route path="/" element={<Navbar />}>
-            {/* <Route path="home" element={<Home />}/> */}
-            <Route path="about" element={<About />}/>
-            {/* <Route path="report Pet" element={<Report_Pet />}/>
-            <Route path="search Pet" element={<Search_Pet />}/> */}
-            {/* <Route path="Login" exact /> */}
-          </Route>
-        </Routes>
+        <Navbar />
+        
+            <Routes>
+              <Route path="/" element={<Home />}/>
+              <Route path="/about" element={<About />}/>
+              <Route path="/report-pet" element={<ReportPet />}/> 
+              <Route path="/search-pet" element={<SearchPet />}/>
+              {/* <Route path="Login" exact /> */}
+            </Routes>
       </BrowserRouter>     
     );
 }
