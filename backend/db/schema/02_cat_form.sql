8bd4dc80823ee78f2cb601205046f1ac48caf91d
Mei Nghiem-Lin <mcnghiem@gmail.com>
2022-01-16 03:57:56 +0000

    rearrange column so image and description is the last 2 columns

--- backend/db/schema/02_cat_form.sql
+++ backend/db/schema/02_cat_form.sql
@@ -3,17 +3,16 @@ DROP TABLE IF EXISTS cat_forms CASCADE;
 CREATE TABLE cat_forms (
 
     id SERIAL PRIMARY KEY NOT NULL,
-    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   
-    description VARCHAR(500) NOT NULL,
-    image TEXT,
     cat_name VARCHAR(255),
     gender VARCHAR(255) NOT NULL,
     last_seen_date DATE NOT NULL,
     last_seen_address VARCHAR(255) NOT NULL,
     last_seen_city VARCHAR(255) NOT NULL,
     last_seen_postal_code VARCHAR(255) NOT NULL,
+    status VARCHAR(20) NOT NULL,
     date_created TIMESTAMP NOT NULL DEFAULT NOW(),
-    status VARCHAR(20) NOT NULL
-
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    image TEXT,
+    description VARCHAR(500) NOT NULL
 ); 
