2fbb3187d2cb1460df868d56e0f7db119a253671
Mei Nghiem-Lin <mcnghiem@gmail.com>
2022-01-16 03:58:40 +0000

    add seed data to all tables

--- backend/db/seeds/01_users.sql
+++ backend/db/seeds/01_users.sql
@@ -1,10 +1,10 @@
 INSERT INTO
-users(id,first_name, last_name, username, email, password)
+users(id,first_name, last_name, username, phone_number, email, password)
 VALUES
-    (1,'Raymond', 'Holt','captainholt','cheddar@nypd99.com', 'password'),
-    (2,'Jake', 'Peralta','jakey99','coolcoolcool@gmail.com', 'password'),
-    (3,'Amy', 'Santiago','turtlebug','hallmonitor@gmail.com', 'password'),
-    (4,'Charles','Boyle','cboyle','charles.boyle@gmail.com', 'password'),
-    (5,'Rosa','Diaz','rosie','ro-ro@gmail.com', 'password'),
-    (6,'Terry','Jeffords','sargehammer','ebonyfalcon@gmail.com', 'password'),
-    (7,'Madeline','Wunsh','mwunsh','captwunsh@gmail.com', 'password');
+    (1,'Raymond', 'Holt','captainholt','416-555-0101','cheddar@nypd99.com', 'password'),
+    (2,'Jake', 'Peralta','jakey99','647-578-0167','coolcoolcool@gmail.com', 'password'),
+    (3,'Amy', 'Santiago','turtlebug','416-874-0107','hallmonitor@gmail.com', 'password'),
+    (4,'Charles','Boyle','cboyle','443-487-1111','charles.boyle@gmail.com', 'password'),
+    (5,'Rosa','Diaz','rosie','416-778-7778','ro-ro@gmail.com', 'password'),
+    (6,'Terry','Jeffords','sargehammer','416-465-0248','ebonyfalcon@gmail.com', 'password'),
+    (7,'Madeline','Wunsh','mwunsh','647-125-5566','captwunsh@gmail.com', 'password');
--- backend/db/seeds/03_comments.sql
+++ backend/db/seeds/03_comments.sql
@@ -1,53 +1,9 @@
 INSERT INTO
-posts(id, comment, date_created, cat_form_id, user_id)
+comments (id, comment, date_created, cat_form_id, user_id)
 VALUES
-    (
-        'Hey! Come back here! You big-a monkey!',
-        'In vel quam orci. Suspendisse potenti. Curabitur eget aliquam ex. Praesent ullamcorper scelerisque egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum viverra magna, ut gravida massa lacinia at. Fusce blandit quis lectus ac iaculis.',
-        1
-    ),
-    (
-        'Nice of the princess to invite us over a picnic, eh Luigi?',
-        'In vel quam orci. Suspendisse potenti. Curabitur eget aliquam ex. Praesent ullamcorper scelerisque egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum viverra magna, ut gravida massa lacinia at. Fusce blandit quis lectus ac iaculis.',
-        1
-    ),
-    (
-        'I wanna be a great plumber like my brother Mario.',
-        'In vel quam orci. Suspendisse potenti. Curabitur eget aliquam ex. Praesent ullamcorper scelerisque egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum viverra magna, ut gravida massa lacinia at. Fusce blandit quis lectus ac iaculis.',
-        2
-    ),
-    (
-        'Luigi, Luigi! Oh yeah! Oh yeah!',
-        'In vel quam orci. Suspendisse potenti. Curabitur eget aliquam ex. Praesent ullamcorper scelerisque egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum viverra magna, ut gravida massa lacinia at. Fusce blandit quis lectus ac iaculis.',
-        2
-    ),
-    (
-        'Did you see Toad and Chao and the sand?',
-        'In vel quam orci. Suspendisse potenti. Curabitur eget aliquam ex. Praesent ullamcorper scelerisque egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum viverra magna, ut gravida massa lacinia at. Fusce blandit quis lectus ac iaculis.',
-        3
-    ),
-    (
-        'Aw, brutal!',
-        'In vel quam orci. Suspendisse potenti. Curabitur eget aliquam ex. Praesent ullamcorper scelerisque egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum viverra magna, ut gravida massa lacinia at. Fusce blandit quis lectus ac iaculis.',
-        3
-    ),
-    (
-        'Whoo-hoo! Yippee!',
-        'In vel quam orci. Suspendisse potenti. Curabitur eget aliquam ex. Praesent ullamcorper scelerisque egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum viverra magna, ut gravida massa lacinia at. Fusce blandit quis lectus ac iaculis.',
-        4
-    ),
-    (
-        'Aww, sweet! I did it, yay!',
-        'In vel quam orci. Suspendisse potenti. Curabitur eget aliquam ex. Praesent ullamcorper scelerisque egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum viverra magna, ut gravida massa lacinia at. Fusce blandit quis lectus ac iaculis.',
-        4
-    ),
-    (
-        'Banana slamma!!',
-        'In vel quam orci. Suspendisse potenti. Curabitur eget aliquam ex. Praesent ullamcorper scelerisque egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum viverra magna, ut gravida massa lacinia at. Fusce blandit quis lectus ac iaculis.',
-        5
-    ),
-    (
-        'Help, Diddy! I ve fallen, and I can t get up! Can you pass me a banana, little buddy?',
-        'In vel quam orci. Suspendisse potenti. Curabitur eget aliquam ex. Praesent ullamcorper scelerisque egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum viverra magna, ut gravida massa lacinia at. Fusce blandit quis lectus ac iaculis.',
-        5
-    );
+(1, 'meow cats are fats i like to pets them they like to meow','2021, 12,26', 1, 1),
+(2, 'Make plans to dominate world and then take a nap i dreamt about fish yum!.','2021, 12,26', 1, 2),
+(3, 'Meow for food, then when human fills food dish, take a few bites of food ','2021, 12,27', 1, 4),
+(4, 'Bite the neighbors bratty kid cat playing a fiddle in hey diddle diddle?','2022, 01,05', 2, 3),
+(5, 'Purrrrrr. Ask to be pet then attack owners hand meow go back to sleep owner brings food and water','2022, 01,04', 2, 2),
+(6, 'Cat ipsum dolor sit amet, freak human out make funny noise mow mow mow mow mow mow success now attack human i is not fat, i is fluffy spend','2022, 01,06', 2, 5);
