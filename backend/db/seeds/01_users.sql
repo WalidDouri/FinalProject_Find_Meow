37159460a142b859a5b212edb5eeeedc57887dc6
Mei Nghiem-Lin <mcnghiem@gmail.com>
2022-01-17 01:30:45 +0000

    remove id colum from seeds since it automatically populates

--- backend/db/seeds/01_users.sql
+++ backend/db/seeds/01_users.sql
@@ -1,10 +1,10 @@
 INSERT INTO
-users(id,first_name, last_name, username, phone_number, email, password)
+users(first_name, last_name, username, phone_number, email, password)
 VALUES
-    (1,'Raymond', 'Holt','captainholt','416-555-0101','cheddar@nypd99.com', 'password'),
-    (2,'Jake', 'Peralta','jakey99','647-578-0167','coolcoolcool@gmail.com', 'password'),
-    (3,'Amy', 'Santiago','turtlebug','416-874-0107','hallmonitor@gmail.com', 'password'),
-    (4,'Charles','Boyle','cboyle','443-487-1111','charles.boyle@gmail.com', 'password'),
-    (5,'Rosa','Diaz','rosie','416-778-7778','ro-ro@gmail.com', 'password'),
-    (6,'Terry','Jeffords','sargehammer','416-465-0248','ebonyfalcon@gmail.com', 'password'),
-    (7,'Madeline','Wunsh','mwunsh','647-125-5566','captwunsh@gmail.com', 'password');
+    ('Raymond', 'Holt','captainholt','416-555-0101','cheddar@nypd99.com', 'password'),
+    ('Jake', 'Peralta','jakey99','647-578-0167','coolcoolcool@gmail.com', 'password'),
+    ('Amy', 'Santiago','turtlebug','416-874-0107','hallmonitor@gmail.com', 'password'),
+    ('Charles','Boyle','cboyle','443-487-1111','charles.boyle@gmail.com', 'password'),
+    ('Rosa','Diaz','rosie','416-778-7778','ro-ro@gmail.com', 'password'),
+    ('Terry','Jeffords','sargehammer','416-465-0248','ebonyfalcon@gmail.com', 'password'),
+    ('Madeline','Wunsh','mwunsh','647-125-5566','captwunsh@gmail.com', 'password');
