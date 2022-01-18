1f5dfd47912b85495a455f7bdae3631fc49cd9c3
Walid <walid_douri@hotmail.com>
2022-01-18 15:43:41 -0500

    Additional work for searchform

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -9,13 +9,21 @@ router.get('/', function (req, res) {
 
   const { last_seen_city, last_seen_postal_code, status } = req.query
   console.log("hello:", last_seen_city);
+  console.log("-----------:", req.params)
 
   // use exact values
   db.query(`
     SELECT 
+    cat_name,
+    gender,
+    age,
+    last_seen_date,
+    last_seen_address,
     last_seen_city,
     last_seen_postal_code,
-    status
+    status,
+    image,
+    description
     FROM cat_forms WHERE 
     last_seen_city LIKE $1 OR
     last_seen_postal_code LIKE $2 OR 
--- frontend/src/Pages/SearchPet.jsx
+++ frontend/src/Pages/SearchPet.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import SearchForm from '../components/SearchForm'
-
+// import '../SearchForm.scss'
 
 export default function ReportPet() {
   return (
     <div>
       {/* <Navbar /> */}
       <h1>THIS IS THE SEARCH PET PAGE</h1>
+      <h2> talk to team about date range for search</h2>
       <SearchForm />
+      <div className="search-result-list">Search Result List</div>
     </div>
   )
 }
