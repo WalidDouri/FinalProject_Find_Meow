ca78ce02df6de78633435a94ac3efde71f2fa05e
Mei Nghiem-Lin <mcnghiem@gmail.com>
2022-01-18 01:33:15 +0000

    no change

--- frontend/src/Pages/SearchPet.jsx
+++ frontend/src/Pages/SearchPet.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import SearchForm from '../components/SearchForm'
+import './SearchPet.scss'
 
 
 export default function ReportPet() {
   return (
-    <div>
+    <div className='searchpet'>
       {/* <Navbar /> */}
       <h1>THIS IS THE SEARCH PET PAGE</h1>
       <SearchForm />
