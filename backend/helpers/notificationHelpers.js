e681a038d97e744c91c6c0ad0f8299d72ec676a0
tsumitakaoru <k.tsumita01@gmail.com>
2022-01-19 17:31:40 -0500

    Add notificationHelper

--- backend/helpers/notificationHelpers.js
+++ backend/helpers/notificationHelpers.js
@@ -3,32 +3,34 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
-const sendNotification = (postId) => {
+const sendSMS = (user) =>  {
+  const { id, first_name, last_name, phone_number } = user;
+  console.log(id, first_name, last_name, phone_number);
+  const msg = `Hi! ${first_name} ${last_name}, This is from Find Me-Ow! You got a comment on your post: ${id}.`;
+  return client.messages
+    .create({
+      body: msg,
+      from: process.env.SOURCE_NUMBER,
+      to: process.env.DESTINATION_NUMBER
+    })
+    .then(message => {
+      console.log(message.sid);
+      return {success: true, msg: `sent SMS for post id ${id}`};
+    });
+};
+
+const createNotification = (postId) => {
 
   const userInfoQuery = `SELECT users.id, first_name, last_name, phone_number, cat_forms.id FROM users JOIN cat_forms ON users.id = cat_forms.user_id WHERE cat_forms.id =$1;`;
   const queryParams = [postId];
-  db.query(userInfoQuery, queryParams)
+  return db.query(userInfoQuery, queryParams)
     .then(data => {
       const user = data.rows[0];
       if (!user) {
-        res.status(403).json({ msg: `Doesn't send notification` });
         throw new Error(`This post doesn't exist.`);
       }
-      const { id, first_name, last_name, phone_number } = user;
-      console.log(id, first_name, last_name, phone_number);
-      const msg = `Hi! ${first_name} ${last_name}, your got a comment on your post: ${id}.`;
-      client.messages
-        .create({
-          body: msg,
-          from: process.env.SOURCE_NUMBER,
-          to: process.env.DESTINATION_NUMBER
-        })
-        .then(message => {
-          res.status(203).json({ msg: `Sent Notification` });
-          console.log(message.sid);
-        });
-    }).catch(err => {
-      console.log(err);
+      return sendSMS(user);
     });
 };
-module.exports = sendNotification;
+
+module.exports = createNotification;
