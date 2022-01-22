b8cf85ac53430c66c33307d4984e433b4d2e3faa
Mei Nghiem-Lin <mcnghiem@gmail.com>
2022-01-22 21:24:00 +0000

    add carousel

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,14 +1,21 @@
+
 import React from 'react'
+import CarouselTest from '../components/Carousel'
 import LoginForm from '../components/LoginForm'
 import './Login.scss'
 
 
 export default function Login() {
   return (
-    <div className='login'>
-      {/* <Navbar /> */}
+    <div id='login'>
+     
       <h1>THIS IS THE Login</h1>
+      <div id="Left" >
       <LoginForm />
+      </div>
+      <div id="Right">
+      <CarouselTest/>
+      </div>
     </div>
   )
 }
