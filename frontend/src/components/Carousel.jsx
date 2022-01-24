7f569643a678b908d016f4c65f6a887ef9f8271a
Mei Nghiem-Lin <mcnghiem@gmail.com>
2022-01-24 07:20:20 +0000

    format pics to be the same size as well as change css

--- frontend/src/components/Carousel.jsx
+++ frontend/src/components/Carousel.jsx
@@ -4,10 +4,10 @@ import './Carousel.scss'
 
 import streetCat from './images/streetCat2525.jpg'
 import siameseCat from './images/siameseCat.jpg'
-import blackCat from './images/blackCat3000x3755.jpg'
+import blackCat from './images/blackCat2525.jpg'
 import pokeCat from './images/pokeCat1.jpg'
 
-import test2 from './images/test2.jpg'
+import walidCat from './images/walidCat25.jpeg'
 
 
 export default function CarouselTest (){
@@ -18,30 +18,33 @@ export default function CarouselTest (){
         <Carousel autoplay>
 
           <div className='photo'>
-            <img src={test2} alt="Grey Cat laying down"
-            width="100%" height="850"
+            <img src={pokeCat} alt="Grey Cat laying down"
+            // width="100%" height="850"
             />
           </div>
 
-          <div className='test'>
+          <div className='photo'>
             <img src={blackCat} alt="Blue Eyed Cat"
-            width="100%" height="850"/>
-          </div>
-          
-          <div className='test2'>
-            <img src={streetCat} alt="Multi Colour Cat"
-            width="100%" height="850"
+            // width="100%" height="850"
             />
           </div>
           
-          <div className='test3'>
+          <div className='photo'>
+            <img src={streetCat} alt="Multi Colour Cat"
+            // width="100%" height="850"
+            />
+          </div>
+          
+          <div className='photo'>
             <img src={siameseCat} alt="Cat in Grass"
-            width="100%" height="850"/>
+            // width="100%" height="850"
+            />
           </div>
 
-          <div className='test4'>
-            <img src={pokeCat} alt="Striped Cat"
-            width="100%" height="850"/> 
+          <div className='photo'>
+            <img src={walidCat} alt="Striped Cat"
+            // width="100%" height="850"
+            /> 
           </div>
         </Carousel>
       </div>
