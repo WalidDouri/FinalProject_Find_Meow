5421102ebe3150bd7a1dbcd6786eb52afee3c942
Mei Nghiem-Lin <mcnghiem@gmail.com>
2022-01-24 02:27:06 +0000

    no changes

--- frontend/src/components/CatFormItem.jsx
+++ frontend/src/components/CatFormItem.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Card } from 'antd'
 import './CatFormItem.scss'
-// import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMars, faCalendar, faVenus } from '@fortawesome/free-solid-svg-icons'
 
