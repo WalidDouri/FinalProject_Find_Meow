98e1f021bbd16a9484df71d489bd0b452eb6fff4
tsumitakaoru <k.tsumita01@gmail.com>
2022-01-24 12:36:07 -0500

    Add PostDetail page

---
+++ frontend/src/components/PostItem.jsx
@@ -0,0 +1,69 @@
+import React, { useContext, useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { Card } from 'antd'
+// import "bootstrap/dist/css/bootstrap.min.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMars, faCalendar, faVenus } from '@fortawesome/free-solid-svg-icons'
+import axios from 'axios';
+import { authContext } from '../providers/Authprovider'
+import Loading from "./Loading";
+
+
+export default function Post() {
+  const { user } = useContext(authContext);
+  const [post, setPost] = useState(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    axios.get(`http://localhost:3001/api/catforms/${id}`)
+      .then(res => {
+        const postData = res.data;
+        setPost(postData);
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [id]);
+
+  if (!post) {
+    return <Loading />
+  }
+  return (
+    <div className="card mb-3" style={{ width: '1000px' }}>
+      <h1>This is Post detail component</h1>
+      <div className="row no-gutters">
+        <div className="col-md-4">
+            <Card hoverable>
+              
+            <img src={post.image} alt="" 
+            style={{width:'100%' ,height:'250px'}}/>
+            
+            <text x="50%" y="50%" fill="#dee2e6" dy=".3em">
+            
+            </text>
+            </Card>
+ 
+        </div>
+        <div className="col-md-8">
+          <div className="card-body">
+            <h5 className="card-title">{post.name}</h5>
+            <p className="card-text">{post.age}&nbsp;months old</p>
+            {post.gender === 'male' ? <p className="card-text"><FontAwesomeIcon icon={faMars}/>&nbsp;{post.gender}</p> : 
+            <p className="card-text"><FontAwesomeIcon icon={faVenus}/>&nbsp;{post.gender}</p>}
+            
+            <p className="card-text"title={post.last_seen_date }>
+            <FontAwesomeIcon icon={faCalendar}/>&nbsp;Last Seen:&nbsp; 
+            {new Intl.DateTimeFormat('en-GB', { 
+                month: 'long', 
+                day: '2-digit',
+                year: 'numeric', 
+            }).format(new Date(post.last_seen_date))}</p>
+            <p className="card-text">
+              <small className="text-muted">{post.description}</small>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
