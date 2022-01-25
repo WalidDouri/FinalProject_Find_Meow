d6145ee9dcaea333dc7c0f8c7b3e5fcaf75f42ce
Mei Nghiem-Lin <mcnghiem@gmail.com>
2022-01-25 23:57:37 +0000

    import scss

--- frontend/src/Pages/PostDetail.jsx
+++ frontend/src/Pages/PostDetail.jsx
@@ -3,10 +3,15 @@ import CatFormTEST from '../components/CatFormTEST'
 import CommentSection from '../components/CommentSection'
 import PostItem from '../components/PostItem'
 
+import './PostDetail.scss'
+
 export default function PostDetail() {
  
   return (
     <>
+    <div className='detailsTitle'>
+    <h1>Cat Details</h1>
+    </div>
     <div className='postDetail'>
       <CatFormTEST/>
     </div>
