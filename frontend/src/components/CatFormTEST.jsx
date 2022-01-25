9021c23e4afc83b5c2d70a52f0799e29c652265f
Mei Nghiem-Lin <mcnghiem@gmail.com>
2022-01-25 21:19:35 +0000

    add styling for CatFormTest.jsx

--- frontend/src/components/CatFormTEST.jsx
+++ frontend/src/components/CatFormTEST.jsx
@@ -1,12 +1,49 @@
-import React from 'react';
-import CatForms from './CatForms';
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios';
+import Loading from "./Loading";
 
-export default function CatFormTEST(props) {
-  console.log("props in catformTEST", props)
+import './CatForms.scss'
+
+
+export default function CatFormTEST() {
+
+  const [post, setPost] = useState(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    axios.get(`http://localhost:3001/api/catforms/${id}`)
+      .then(res => {
+        const postData = res.data;
+        setPost(postData);
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [id]);
+
+  if (!post) {
+    return <Loading />
+  }
+  
   return (
+    <>
     <div className='box'> 
-      <img src={props.image}/>
+      <img src={post.image}/>
     </div>
+      <div className='overThis'>
+        <div className='boxDetails'>
+          <p>{post.cat_name} </p>
+          <p>{post.status} </p>
+          <p>{post.description}</p>
+        </div>
+      <div className='boxMap'>
+          <p> TEST</p>
+          </div>
+        </div>
+      
+    
+    </>
   
   )
 }
