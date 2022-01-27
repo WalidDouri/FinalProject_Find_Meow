92d0d8173470fdb250e76761f750ed7d2cc4a530
Mei Nghiem-Lin <mcnghiem@gmail.com>
2022-01-27 04:48:08 +0000

    added new seed to data

--- backend/db/seeds/03_comments.sql
+++ backend/db/seeds/03_comments.sql
@@ -6,4 +6,22 @@ VALUES
 ('Meow for food, then when human fills food dish, take a few bites of food ','2021, 12,27', 1, 4),
 ('Bite the neighbors bratty kid cat playing a fiddle in hey diddle diddle?','2022, 01,05', 2, 3),
 ('Purrrrrr. Ask to be pet then attack owners hand meow go back to sleep owner brings food and water','2022, 01,04', 2, 2),
-('Cat ipsum dolor sit amet, freak human out make funny noise mow mow mow mow mow mow success now attack human i is not fat, i is fluffy spend','2022, 01,06', 2, 5);
+('Cat ipsum dolor sit amet, freak human out make funny noise mow mow mow mow mow mow success now attack human i is not fat, i is fluffy spend','2022, 01,06', 2, 5),
+('Bite the neighbors bratty kid cat playing a fiddle in hey diddle diddle?','2022, 01,05', 2, 3),
+
+
+('Cred messenger bag bicycle rights selvage','2022, 01,06', 4, 7),
+('Try-hard paleo pour-over, banh mi synth lo-fi chambray direct','2022, 01,06', 4, 8),
+('Four dollar toast banh mi craft beer vinyl slow-carb tumeric williamsburg hammock affogato trust fund gastropub freegan','2022, 01,05', 5, 9),
+('YOLO scenester godard. Live-edge 90s selfies salvia echo park.','2022, 01,06', 5, 10),
+('Selvage pitchfork vaporware unicorn chillwave.','2022, 01,06', 6, 11),
+('Dummy text? More like dummy thicc text, amirite?','2022, 01,05', 6, 12),
+('have not heard of them locavore lomo tilde. Cliche +1','2022, 01,06', 7, 13),
+('charcoal fingerstache leggings, green juice','2022, 01,06', 7, 15),
+
+('meow cats are fats i like to pets them they like to meow','2021, 12,26', 8, 2),
+('Make plans to dominate world and then take a nap i dreamt about fish yum!.','2021, 12,26', 8, 2),
+('Meow for food, then when human fills food dish, take a few bites of food ','2021, 12,27', 9, 4),
+('Bite the neighbors bratty kid cat playing a fiddle in hey diddle diddle?','2022, 01,05', 9, 3),
+('Purrrrrr. Ask to be pet then attack owners hand meow go back to sleep owner brings food and water','2022, 01,04', 10, 2),
+('Bite the neighbors bratty kid cat playing a fiddle in hey diddle diddle?','2022, 01,05', 10, 3);
